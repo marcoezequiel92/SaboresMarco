4a432e67155949e6c487f42bb4eeef2267215c25
marcoezequiel92 <marcoezequielsuarez92@gmail.com>
2019-11-19 21:55:03 -0300

    Primera version

---
+++ src/saboresmarco/SaboresMarco.java
@@ -0,0 +1,14 @@
+
+package saboresmarco;
+
+public class SaboresMarco {
+
+    public static void main(String[] args) {
+        String[] sabores = {"Dulce","Salado","Amargo","Acido","Umami","Picante","Astringente","Starchy","Adiposo"};
+        for(int i=0;i<6;i++){
+            System.out.println("Sabor: "+ sabores[i]);
+        }
+    }
+    
+    
+}
